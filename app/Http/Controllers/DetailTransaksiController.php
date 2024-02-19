47afb4bd76c4ba6eff1a31aa594ca0c7875ed62d
keziyol <berlianikpb29@gmail.com>
2024-02-19 13:49:02 +0700

    Making Migration Done!

---
+++ app/Http/Controllers/DetailTransaksiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detail_Transaksi;
+use App\Http\Requests\StoreDetail_TransaksiRequest;
+use App\Http\Requests\UpdateDetail_TransaksiRequest;
+
+class DetailTransaksiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDetail_TransaksiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Detail_Transaksi $detail_Transaksi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Detail_Transaksi $detail_Transaksi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDetail_TransaksiRequest $request, Detail_Transaksi $detail_Transaksi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Detail_Transaksi $detail_Transaksi)
+    {
+        //
+    }
+}
--- database/migrations/2024_02_19_064542_create_detail__transaksis_table.php
+++ database/migrations/2024_02_19_064542_create_detail__transaksis_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('detail__transaksis', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('level')->nullable();
-            $table->rememberToken();
+            $table->string('id_barang')->nullable();
+            $table->string('nota')->nullable();
+            $table->double('count')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('detail__transaksis');
     }
 };
